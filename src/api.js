0bd99e88af642c4c0e7d5e1425cc2587018b2359
Vivek Kumar shaw <vivekshaw8420@gmail.com>
2025-09-09 14:52:17 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000" });
+const API = axios.create({ baseURL: "https://authentication-authorization-phi.vercel.app/" });
 
 export const login = (data) => API.post("/auth/login", data);
 export const register = (data) => API.post("/auth/register", data);
